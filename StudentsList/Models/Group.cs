0acff7b9ac1ea00587e07b6e8494f46ab20a7181
evilvegetarian <66737576+evilvegetarian@users.noreply.github.com>
2021-08-09 17:21:32 +0500

    Add Models

---
+++ StudentsList/Models/Group.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentsList.Models
+{
+    public class Group
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string GroupName { get; set; }
+
+        public List<Student> Students { get; set; }
+
+    }
+}
---
+++ StudentsList/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsList.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public int GroupId { get; set; }
+
+        public Group Group { get; set; }
+
+
+    }
+}
